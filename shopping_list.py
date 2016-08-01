1a504f8f3ca8d9bbbdb192f393051bbd3611cff2
Andreza Feu <andrezafeu@gmail.com>
2016-08-01 18:02:14 -0400

    Add commands SHOW and HELP

--- shopping_list.py
+++ shopping_list.py
@@ -1,15 +1,32 @@
+shopping_list = []
+
 def add_item():
 	print("What would you like to add to your shopping list?")
-	print("Enter 'DONE' when you finish adding items")
-	shopping_list = []
+	show_commands()
+
 	while True:
-		item = input("Item: ")
+		item = input("> ").upper()
 
 		if item == "DONE":
 			break
 
-		shopping_list.append(item)
+		if item == "SHOW":
+			print_list()
+
+		elif item == "HELP":
+			show_commands()
+		else:
+			shopping_list.append(item)
+			print("Added {}. The list now has {} item(s)".format(item, len(shopping_list)))
 		
+	print_list()	
+
+def show_commands():
+	print("""Type 'SHOW' to see items in shopping list
+Type 'HELP' to see these commands again
+Type 'DONE' to quit the app""")
+
+def print_list():
 	print("Shopping List:")
 	print(shopping_list)
 
